3e3467be3cadcfbf3987ddfd9c28a5aef32fcd58
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-08 15:03:13 -0300

    Feat: Implementa a função GET.

--- project-01/src/server.js
+++ project-01/src/server.js
@@ -9,6 +9,16 @@
 import http from 'node:http';
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/users') {
+    return response.end('Listagem de usuários');
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return response.end('Criando um usuário');
+  }
+
   response.end('Hello World');
 });
 
