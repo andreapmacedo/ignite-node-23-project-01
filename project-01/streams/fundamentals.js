88e8d940e21e5245abd968686c31f67f26b0ac4f
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-10 12:45:30 -0300

    Adiciona detalhes de funcionalidade do Node.

---
+++ project-01/streams/fundamentals.js
@@ -0,0 +1,17 @@
+
+
+// Importação de clientes via CSV(excel)
+
+// 1gb -  1.000.000
+
+// POST /upload import.csv
+
+// 10mb/s - 100s
+
+// 100s -> inserção de registros no banco de dados
+
+// 10mb/s -> 10.000 registros
+
+// Readable Streams / Writable Streams
+
+
