cbdca1a36f9999c35d1cf5933fcfdfe554bb5f10
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-15 09:34:49 -0300

    Feat: Adiciona explicação dos paramatros.

--- project-01/src/server.js
+++ project-01/src/server.js
@@ -1,7 +1,27 @@
-
 import http from 'node:http';
 import { json } from '../middlewares/json.js';
 const  { routes } = './routes.js'
+
+// Query Paramaters -> URL Stateful
+// parametro nomeado -> query paramater
+// São usados para filtros, paginação... (dados não sensíveis)
+// http://localhost:3333/users?userID=1 (Query Paramater -> chave: userID e valor: 1)
+// http://localhost:3333/users?userID=1&name=Diego
+
+
+// Route Paramenters
+// parametros não nomeados -> route paramenter
+// São usados para identificar recursos (dados não sensíveis)
+// GET http://localhost:3333/users/1 (Route Paramenter -> chave: userID e valor: 1)
+// DELETE http://localhost:3333/users/1
+// É feito uma combinação de método, recurso e route paramenter para identificar uma rota
+// metodo: GET, recurso: users, route paramenter: 1 
+
+// Request Body
+// envio de informações de formulário
+
+
+
 
 const server = http.createServer(async(req, res) => {
   const { method, url } = req;
