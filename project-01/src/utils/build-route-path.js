1d783689b4d6d97988d6cec9aaeb6ebc6877b3a9
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-15 10:48:48 -0300

    Feat: Adiciona rota com parametro

--- project-01/src/utils/build-route-path.js
+++ project-01/src/utils/build-route-path.js
@@ -3,5 +3,9 @@ export function buildRoutePath(path) {
   // [a-zA-Z] -> depois, pode ter letras de a a z ou de A a Z 
   // + -> uma ou mais vezes
   const routeParametersRegex = /:([a-zA-Z])+/g;
-  console.log(Array.from(path.matchAll(routeParametersRegex)));
+  const paramsWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+  // console.log(Array.from(path.matchAll(routeParametersRegex)));
+  const pathRegex = new RegExp(`^${paramsWithParams}`)
+
+  return pathRegex
 }
