814fe1ba5029547591d30f0803fc2783741a5ddb
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-17 09:23:13 -0300

    Feat: Adiciona filtro de lista de banco de dados.

--- project-01/src/routes.js
+++ project-01/src/routes.js
@@ -11,7 +11,14 @@ export const routes = [
     path: buildRoutePath('/users'),
     handler: (req, res) => {
       console.log(req.query)
-      const users = database.select('users');
+      const { search } = req.query
+      // const users = database.select('users');
+
+      const users = database.select('users', search ? {
+        name: search,
+        email: search
+      } : null)
+
       return res
       .end(JSON.stringify(users));
     }
